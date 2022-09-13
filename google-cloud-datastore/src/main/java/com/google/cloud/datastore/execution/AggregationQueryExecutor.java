2103b800caed606d5a9c79d4c1db3b4f78b6991e
Prateek Jain <jainsahab@google.com>
2022-09-13 13:47:10 +0530

    Implementing aggregation query executor to execute AggergationQuery

---
+++ google-cloud-datastore/src/main/java/com/google/cloud/datastore/execution/AggregationQueryExecutor.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.datastore.execution;
+
+import com.google.cloud.datastore.AggregationQuery;
+import com.google.cloud.datastore.AggregationResults;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.execution.request.AggregationQueryRequestProtoPreparer;
+import com.google.cloud.datastore.execution.response.AggregationQueryResponseTransformer;
+import com.google.cloud.datastore.spi.v1.DatastoreRpc;
+import com.google.datastore.v1.RunAggregationQueryRequest;
+import com.google.datastore.v1.RunAggregationQueryResponse;
+
+public class AggregationQueryExecutor implements QueryExecutor<AggregationQuery, AggregationResults>{
+
+  private final DatastoreRpc datastoreRpc;
+  private final AggregationQueryRequestProtoPreparer protoPreparer;
+  private final AggregationQueryResponseTransformer responseTransformer;
+
+  public AggregationQueryExecutor(DatastoreRpc datastoreRpc, DatastoreOptions datastoreOptions) {
+    this.datastoreRpc = datastoreRpc;
+    this.protoPreparer = new AggregationQueryRequestProtoPreparer(datastoreOptions);
+    this.responseTransformer = new AggregationQueryResponseTransformer();
+  }
+
+  @Override
+  public AggregationResults execute(AggregationQuery query) {
+    RunAggregationQueryRequest runAggregationQueryRequest = this.protoPreparer.prepare(query);
+    RunAggregationQueryResponse runAggregationQueryResponse = this.datastoreRpc.runAggregationQuery(
+        runAggregationQueryRequest);
+    return this.responseTransformer.transform(runAggregationQueryResponse);
+  }
+}
---
+++ google-cloud-datastore/src/test/java/com/google/cloud/datastore/execution/AggregationQueryExecutorTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.datastore.execution;
+
+import static com.google.cloud.datastore.ProtoTestData.intValue;
+import static com.google.cloud.datastore.StructuredQuery.PropertyFilter.eq;
+import static com.google.cloud.datastore.aggregation.Aggregation.count;
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+
+import com.google.cloud.datastore.AggregationQuery;
+import com.google.cloud.datastore.AggregationResults;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.EntityQuery;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.spi.v1.DatastoreRpc;
+import com.google.datastore.v1.AggregationResultBatch;
+import com.google.datastore.v1.RunAggregationQueryRequest;
+import com.google.datastore.v1.RunAggregationQueryResponse;
+import com.google.datastore.v1.Value;
+import java.util.HashMap;
+import java.util.Map;
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AggregationQueryExecutorTest {
+
+  private static final String KIND = "Task";
+  private static final String NAMESPACE = "ns";
+
+  private DatastoreRpc mockRpc;
+  private AggregationQueryExecutor queryExecutor;
+  private DatastoreOptions datastoreOptions;
+
+  @Before
+  public void setUp() throws Exception {
+    mockRpc = EasyMock.createStrictMock(DatastoreRpc.class);
+    datastoreOptions = DatastoreOptions.newBuilder().setNamespace(NAMESPACE).build();
+    queryExecutor = new AggregationQueryExecutor(mockRpc, datastoreOptions);
+  }
+
+  @Test
+  public void shouldExecuteAggregationQuery() {
+    EntityQuery nestedQuery = Query.newEntityQueryBuilder()
+        .setNamespace(NAMESPACE)
+        .setKind(KIND)
+        .setFilter(eq("done", true))
+        .build();
+
+    AggregationQuery aggregationQuery = Query.newAggregationQueryBuilder()
+        .setNamespace(NAMESPACE)
+        .addAggregation(count().as("total"))
+        .over(nestedQuery)
+        .build();
+
+    expect(mockRpc.runAggregationQuery(anyObject(RunAggregationQueryRequest.class))).andReturn(dummyAggregationQueryResponse());
+
+    replay(mockRpc);
+
+    AggregationResults aggregationResults = queryExecutor.execute(aggregationQuery);
+
+    verify(mockRpc);
+    System.out.println(aggregationResults);
+  }
+
+  private RunAggregationQueryResponse dummyAggregationQueryResponse() {
+    Map<String, Value> result1 = new HashMap<String, Value>() {{
+      put("count", intValue(209));
+      put("count_upto_100", intValue(100));
+    }};
+
+    Map<String, Value> result2 = new HashMap<String, Value>() {{
+      put("count", intValue(509));
+      put("count_upto_100", intValue(100));
+    }};
+
+    AggregationResultBatch resultBatch = AggregationResultBatch.newBuilder()
+        .addAggregationResults(com.google.datastore.v1.AggregationResult.newBuilder()
+            .putAllAggregateProperties(result1).build())
+        .addAggregationResults(com.google.datastore.v1.AggregationResult.newBuilder()
+            .putAllAggregateProperties(result2).build())
+        .build();
+    return RunAggregationQueryResponse.newBuilder()
+        .setBatch(resultBatch)
+        .build();
+  }
+}
