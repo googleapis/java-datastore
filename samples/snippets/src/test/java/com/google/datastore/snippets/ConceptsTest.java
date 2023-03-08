885454de3e6b2e0992290e7dccaaa99a4f832b0d
Prateek Jain <jainsahab@google.com>
2023-03-08 18:13:05 +0530

    fixing indentation issue

--- samples/snippets/src/test/java/com/google/datastore/snippets/ConceptsTest.java
+++ samples/snippets/src/test/java/com/google/datastore/snippets/ConceptsTest.java
@@ -1159,8 +1159,8 @@ public class ConceptsTest {
   @Test
   public void testStaleReads() throws InterruptedException {
     setUpQueryTestsRealBackend();
-    TimeUnit.SECONDS.sleep(6); // waiting for 6 seconds, so that we can query
-                                       // with read time of 5 seconds ago
+    // waiting for 6 seconds, so that we can query with read time of 5 seconds ago
+    TimeUnit.SECONDS.sleep(6);
     Datastore datastoreClient = datastoreRealBackend;
     // [START datastore_stale_read]
     Key taskKey =
