8c4a83239bd85ad7e46ccd7c1bfc3ba1e85b3846
Jimit Shah <57637300+jimit-j-shah@users.noreply.github.com>
2024-05-23 10:46:31 -0700

    feat: RunAggregationQuery instrumentation (#1447)
    * feat: RunQuery trace instrumentation
    
    * Formatting
    
    * Formatting
    
    * Refactor: s/RUNQUERY/RUN_QUERY
    
    * feat: RunAggregationQuery Trace Instrumentation
    
    * Build: retiring test assertions for OpenCensus spans - will be replacing this in hermetic integration tests for OpenTelemetry using in-memory span exports (in addition to ITE2ETraceTest.java).
    
    * Formatting
    
    * Fixing @Test annotation missed after merge
    
    * Formatting

--- google-cloud-datastore/src/main/java/com/google/cloud/datastore/telemetry/TraceUtil.java
+++ google-cloud-datastore/src/main/java/com/google/cloud/datastore/telemetry/TraceUtil.java
@@ -34,6 +34,7 @@ public interface TraceUtil {
   static final String SPAN_NAME_LOOKUP = "Lookup";
   static final String SPAN_NAME_COMMIT = "Commit";
   static final String SPAN_NAME_RUN_QUERY = "RunQuery";
+  static final String SPAN_NAME_RUN_AGGREGATION_QUERY = "RunAggregationQuery";
 
   /**
    * Creates and returns an instance of the TraceUtil class.
