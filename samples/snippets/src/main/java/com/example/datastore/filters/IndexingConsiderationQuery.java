fa81c2a8acc8f96fd2abfd1c9975166f6d3d4af5
Cindy Peng <148148319+cindy-peng@users.noreply.github.com>
2024-09-18 10:21:58 -0700

    fix formatting

--- samples/snippets/src/main/java/com/example/datastore/filters/IndexingConsiderationQuery.java
+++ samples/snippets/src/main/java/com/example/datastore/filters/IndexingConsiderationQuery.java
@@ -18,7 +18,8 @@ package com.example.datastore.filters;
 
 // sample-metadata:
 //   title: Queries with indexing considerations
-//   description: The following query produces a result set that is ordered according to the index definition.
+//   description: The following query produces a result set
+//   that is ordered according to the index definition.
 
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
@@ -30,34 +31,34 @@ import com.google.cloud.datastore.StructuredQuery.Filter;
 import com.google.cloud.datastore.StructuredQuery.PropertyFilter;
 
 public class IndexingConsiderationQuery {
-    public static void invoke() throws Exception {
+  public static void invoke() throws Exception {
 
-        // Instantiates a client
-        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    // Instantiates a client
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
 
-        // Build a query with multi inequal filters and optimized index order of index properties.
-        // [START datastore_query_indexing_considerations]
-        Query<Entity> query = Query.newEntityQueryBuilder()
-                .setKind("employees")
-                .setFilter(CompositeFilter.and(PropertyFilter.gt("salary", 100000), PropertyFilter.gt("experience", 0)))
-                .setOrderBy(OrderBy("salary"), OrderBy("experience"))
-                .build();
-        // [END datastore_query_indexing_considerations]
+    // Build a query with multi inequal filters and optimized index order of index properties.
+    // [START datastore_query_indexing_considerations]
+    Query<Entity> query = Query.newEntityQueryBuilder()
+            .setKind("employees")
+            .setFilter(CompositeFilter.and(PropertyFilter.gt("salary", 100000), PropertyFilter.gt("experience", 0)))
+            .setOrderBy(OrderBy("salary"), OrderBy("experience"))
+            .build();
+    // [END datastore_query_indexing_considerations]
 
-        // Get the results back from Datastore
-        QueryResults<Entity> results = datastore.run(query);
+    // Get the results back from Datastore
+    QueryResults<Entity> results = datastore.run(query);
 
-        if (!results.hasNext()) {
-            throw new Exception("query yielded no results");
-        }
-
-        while (results.hasNext()) {
-            Entity entity = results.next();
-            System.out.printf("Entity: %s%n", entity);
-        }
-
-        AggregationResult customer1SalesAvgQueryResult = Iterables.getOnlyElement(datastore.runAggregation(customer1SalesAvg));
-
-        System.out.printf("Customer 1 sales avg is %d", customer1SalesAvgQueryResult.getLong("total_sales")); // 92
+    if (!results.hasNext()) {
+      throw new Exception("query yielded no results");
     }
+
+    while (results.hasNext()) {
+      Entity entity = results.next();
+      System.out.printf("Entity: %s%n", entity);
+    }
+
+    AggregationResult customer1SalesAvgQueryResult = Iterables.getOnlyElement(datastore.runAggregation(customer1SalesAvg));
+
+    System.out.printf("Customer 1 sales avg is %d", customer1SalesAvgQueryResult.getLong("total_sales")); // 92
+  }
 }
